edd1131c100319169cf622bbe181e7de6de581a0
Hannah O'Keeffe <hannah.okeeffe00@gmail.com>
2026-07-26 15:57:18 +0100

    tidied up files, moved old to old untracked, removed from github

---
+++ minou_colors.py
@@ -0,0 +1,10 @@
+"""Shared Minou colour palette for all notebooks."""
+
+MINOU_COLORS = {
+    "teal": "#00798C",
+    "coral": "#D1495B",
+    "gold": "#EDAE49",
+    "sage": "#66A182",
+    "navy": "#2E4057",
+    "grey": "#8D96A3",
+}
